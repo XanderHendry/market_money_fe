bc412775f33f162ff83fa998b987974e0f72e352
Xander Hendry <vhendry.coding@gmail.com>
2023-12-14 13:04:41 -0500

    add vendor poro

---
+++ app/poros/vendor.rb
@@ -0,0 +1,20 @@
+class Vendor
+  attr_reader :id,
+              :name,
+              :description,
+              :contact_name,
+              :contact_phone
+
+  def initialize(vendor_details)
+    @id = vendor_details[:id]
+    @name = vendor_details[:name]
+    @description = vendor_details[:description]
+    @contact_name = vendor_details[:contact_name]
+    @contact_phone = vendor_details[:contact_phone]
+    @credit_accepted = vendor_details[:credit_accepted]
+  end
+
+  def credit_accepted?
+    @credit_accepted
+  end
+end
---
+++ spec/poros/vendor_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Vendor do
+  it 'exists' do
+    vendor = Vendor.new({id: "55823", name: "The Charcuterie Corner", description: "Vendor selling a variety of artisanal cured meats and sausages.", contact_name: "Claudie Langworth III", contact_phone: "1-147-179-9747", credit_accepted: false})
+
+    expect(vendor).to be_a(Vendor)
+    expect(vendor).to respond_to(:id)
+    expect(vendor).to respond_to(:name)
+    expect(vendor).to respond_to(:description)
+    expect(vendor).to respond_to(:contact_name)
+    expect(vendor).to respond_to(:contact_phone)
+  end
+
+  describe '#credit_accepted?' do
+    it 'returns a boolean value for if a vendor accepts credit.' do
+      vendor = Vendor.new({id: "55823", name: "The Charcuterie Corner", description: "Vendor selling a variety of artisanal cured meats and sausages.", contact_name: "Claudie Langworth III", contact_phone: "1-147-179-9747", credit_accepted: false})
+      expect(vendor.credit_accepted?).to eq(false)
+    end
+  end
+end 
