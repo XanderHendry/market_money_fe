6187b7288babae4e7b6569a4a6710e9b3088b431
Xander Hendry <vhendry.coding@gmail.com>
2023-12-14 11:21:49 -0500

    add market facade spec

---
+++ spec/facades/market_facade_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe MarketFacade do
+  describe '#all_markets' do
+    it 'converts a full list of Markets from parsed JSON to Market POROs', :vcr do
+      poros = MarketFacade.new.all_markets
+      expect(poros).to be_an(Array)
+      expect(poros.first).to be_a(Market)
+      expect(poros.first).to respond_to(:id)
+      expect(poros.first).to respond_to(:name)
+      expect(poros.first).to respond_to(:street)
+      expect(poros.first).to respond_to(:city)
+      expect(poros.first).to respond_to(:county)
+      expect(poros.first).to respond_to(:state)
+      expect(poros.first).to respond_to(:zip)
+      expect(poros.first).to respond_to(:lon)
+      expect(poros.first).to respond_to(:lat)
+      expect(poros.first).to respond_to(:vendor_count)
+    end
+  end
+  describe '#get_market' do
+  it 'converts a single Market from parsed JSON to Market POROs', :vcr do
+    market = MarketFacade.new.all_markets
+    expect(market).to be_a(Market)
+    expect(market).to respond_to(:id)
+    expect(market).to respond_to(:name)
+    expect(market).to respond_to(:street)
+    expect(market).to respond_to(:city)
+    expect(market).to respond_to(:county)
+    expect(market).to respond_to(:state)
+    expect(market).to respond_to(:zip)
+    expect(market).to respond_to(:lon)
+    expect(market).to respond_to(:lat)
+    expect(market).to respond_to(:vendor_count)
+  end
+  end
+end 
